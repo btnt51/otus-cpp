2ab9fc3a269906fc08f203366280f98efa342cd4
Evgeniy btnt51 <prog@btnt51.ru>
2023-01-24 00:04:01 +0300

    Initial commit of this brench. Added two first hash functions. Testing github action instaling boost and build proj

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include "boost/uuid/uuid.hpp"
+#include "boost/uuid/detail/sha1.hpp"
+#include "boost/crc.hpp"
 
-int main(int, char **) {
+
+std::string get_sha1(const std::string& p_arg)
+{
+    boost::uuids::detail::sha1 sha1;
+    sha1.process_bytes(p_arg.data(), p_arg.size());
+    unsigned hash[5] = {0};
+    sha1.get_digest(hash);
+
+    // Back to string
+    char buf[41] = {0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::snprintf(buf + (i << 3), 40, "%08x", hash[i]);
+    }
+
+    return std::string(buf);
+}
+
+uint32_t GetCrc32(const std::string& my_string) {
+    boost::crc_32_type result;
+    result.process_bytes(my_string.data(), my_string.length());
+    return result.checksum();
+}
+
+int main(int argc, char ** argv) {
 	try {
+		if (argc < 2) {
+      		std::cerr << "Call: " << *argv << " STR\n";
+      		return 1;
+		}
+
+		std::cout << get_sha1(argv[1]) << '\n';
+
+		std::cout << GetCrc32(argv[1]) << '\n';
 
 	} 
 	catch(std::exception &e) {
