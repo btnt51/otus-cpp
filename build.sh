2ce59a9c6863f2437db4ef4c43ad7221e7c2601a
Evgeniy btnt51 <prog@btnt51.ru>
2023-01-25 23:36:28 +0300

    New fixes for build  and release files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir ./build
 cd ./build
-cmake -DPATCH_VERSION=$1 -DWITH_TESTS=$2 ../
+cmake -DPATCH_VERSION=$1 -DWITH_TESTS=$2 -DBoost_INCLUDE_DIR=$4 -DBoost_LIBRARY_DIRS=$5 -DBoost_ROOT=$6 ../
 cmake --build .
 cmake --build . --target $3
 
